aec5564ec27f24958f75d03eb9caa0b229f69041
Hydra <hydra@aleph.engineering>
2020-10-31 02:26:03 -0400

    Print information from a static file in the local storage

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,24 @@
 package main;
 
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
         // Receive a TXT
+        try {
+            File auditTxt = new File("/home/aleph02/Documents/audit.txt");
+            Scanner myReader = new Scanner(auditTxt);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
 
         // Convert to Auditoria
 
