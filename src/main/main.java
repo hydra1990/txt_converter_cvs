a0c94febc9a55f796042a7b0eec35442c41517ea
Hydra <hydra@aleph.engineering>
2020-10-31 01:34:21 -0400

    State the algorithm for what I want to do

--- src/main/main.java
+++ src/main/main.java
@@ -3,5 +3,11 @@ package main;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // Receive a PDF
+
+        // Convert to Auditoria
+
+        // Return a CSV
+
     }
 }
