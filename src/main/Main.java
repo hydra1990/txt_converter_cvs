2e2e1fa6163c3c4ebe410cafd2d4f07e5d08bf3e
Hydra <hydra@aleph.engineering>
2020-10-31 09:34:32 -0400

    Move the converter code to a Converter class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,70 +1,9 @@
 package main;
 
-import domain.Auditoria;
-
-import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
-    List<Auditoria> auditorias = new ArrayList<>();
-
     public static void main(String[] args) {
-        File auditTxt = getAuditTxt();
-        addAuditorias(auditTxt);
+        Converter converter = new Converter();
+        converter.convert();
     }
-
-    private static File getAuditTxt() {
-        JFileChooser fileChooser = new JFileChooser();
-        FileFilter txtFilter = new FileNameExtensionFilter("Plain Text Files", "txt");
-        fileChooser.setAcceptAllFileFilterUsed(false);
-        fileChooser.setFileFilter(txtFilter);
-
-        int returnVal = fileChooser.showOpenDialog(null);
-
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            return fileChooser.getSelectedFile();
-        }
-
-        return null;
-    }
-
-    private static void addAuditorias(File auditTxt) {
-        try (Scanner myReader = new Scanner(auditTxt)) {
-            if (myReader.hasNextLine()) {
-                myReader.nextLine();
-            }
-            while (myReader.hasNextLine()) {
-                String dataLine = myReader.nextLine();
-                transformAuditoria(dataLine);
-            }
-        } catch (FileNotFoundException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
-    }
-
-    // Convert to Auditoria
-    private static void transformAuditoria(String cuenta) {
-        String[] information = cuenta.split("(^\"|\",\"|,)");
-        Auditoria auditoria = new Auditoria();
-        auditoria.setCuenta(information[1]);
-        auditoria.setDescripcion(information[2]);
-        auditoria.setEntidad(information[3]);
-        auditoria.setAct(information[4]);
-        auditoria.setSaldoApertura(new BigDecimal(information[5]));
-        auditoria.setSaldoInicial(new BigDecimal(information[6]));
-        auditoria.setDebito(new BigDecimal(information[7]));
-        auditoria.setCredito(new BigDecimal(information[8]));
-        auditoria.setSaldoFinal(new BigDecimal(information[9]));
-
-    }
-
-    // Return a CSV
 }
