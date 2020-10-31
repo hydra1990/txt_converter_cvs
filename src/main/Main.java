ac110dac933e16f4c714e643472e43d6b53ac344
Hydra <hydra@aleph.engineering>
2020-10-31 03:21:22 -0400

    Refactor the code, divide it into smaller methods Add an auditorias list, so we can manage them globally

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,39 +1,57 @@
 package main;
 
+import domain.Auditoria;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    List<Auditoria> auditorias = new ArrayList<>();
+
     public static void main(String[] args) {
-        // Receive a TXT
-        try {
-            JFileChooser fileChooser = new JFileChooser();
-            FileFilter txtFilter = new FileNameExtensionFilter("Plain Text Files", "txt");
-            fileChooser.setAcceptAllFileFilterUsed(false);
-            fileChooser.setFileFilter(txtFilter);
+        File auditTxt = getAuditTxt();
+        addAuditorias(auditTxt);
+    }
 
-            int returnVal = fileChooser.showOpenDialog(null);
+    private static File getAuditTxt() {
+        JFileChooser fileChooser = new JFileChooser();
+        FileFilter txtFilter = new FileNameExtensionFilter("Plain Text Files", "txt");
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setFileFilter(txtFilter);
 
-            if (returnVal == JFileChooser.APPROVE_OPTION){
-                File auditTxt = fileChooser.getSelectedFile();
-                Scanner myReader = new Scanner(auditTxt);
-                while (myReader.hasNextLine()) {
-                    String data = myReader.nextLine();
-                    System.out.println(data);
-                }
-                myReader.close();
+        int returnVal = fileChooser.showOpenDialog(null);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            return fileChooser.getSelectedFile();
+        }
+
+        return null;
+    }
+
+    private static void addAuditorias(File auditTxt) {
+        try (Scanner myReader = new Scanner(auditTxt)) {
+            while (myReader.hasNextLine()) {
+                String dataLine = myReader.nextLine();
+                transformAuditoria(dataLine);
             }
         } catch (FileNotFoundException e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
-
-        // Convert to Auditoria
-
-        // Return a CSV
-
     }
+
+    // Convert to Auditoria
+    private static void transformAuditoria(String cuenta) {
+        String[] information = cuenta.split("\",\"");
+        System.out.println(Arrays.toString(information));
+    }
+
+    // Return a CSV
 }
