985569eebd3b9800073f78cc71597f7fde98d5fc
Hydra <hydra@aleph.engineering>
2020-10-31 01:32:57 -0400

    Add getter and setter for Auditoria

--- src/domain/Auditoria.java
+++ src/domain/Auditoria.java
@@ -10,4 +10,76 @@ public class Auditoria {
     private long debito;
     private long credito;
     private long saldoFinal;
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEntidad() {
+        return entidad;
+    }
+
+    public void setEntidad(String entidad) {
+        this.entidad = entidad;
+    }
+
+    public String getAct() {
+        return act;
+    }
+
+    public void setAct(String act) {
+        this.act = act;
+    }
+
+    public long getSaldoApertura() {
+        return saldoApertura;
+    }
+
+    public void setSaldoApertura(long saldoApertura) {
+        this.saldoApertura = saldoApertura;
+    }
+
+    public long getSaldoInicial() {
+        return saldoInicial;
+    }
+
+    public void setSaldoInicial(long saldoInicial) {
+        this.saldoInicial = saldoInicial;
+    }
+
+    public long getDebito() {
+        return debito;
+    }
+
+    public void setDebito(long debito) {
+        this.debito = debito;
+    }
+
+    public long getCredito() {
+        return credito;
+    }
+
+    public void setCredito(long credito) {
+        this.credito = credito;
+    }
+
+    public long getSaldoFinal() {
+        return saldoFinal;
+    }
+
+    public void setSaldoFinal(long saldoFinal) {
+        this.saldoFinal = saldoFinal;
+    }
 }
