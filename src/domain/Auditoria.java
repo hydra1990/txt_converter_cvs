75e176090e19265b7b88c0bbbc9f95ace93ab1f4
Hydra <hydra@aleph.engineering>
2020-10-31 01:32:20 -0400

    Add the object Auditoria with the necessary attributes

---
+++ src/domain/Auditoria.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Auditoria {
+    private String cuenta;
+    private String descripcion;
+    private String entidad;
+    private String act;
+    private long saldoApertura;
+    private long saldoInicial;
+    private long debito;
+    private long credito;
+    private long saldoFinal;
+}
