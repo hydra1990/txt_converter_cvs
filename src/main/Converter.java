a26130670cbfa73aec9dfe0abdd8ee67a26ed743
Hydra <hydra@aleph.engineering>
2020-11-01 14:46:33 -0500

    Throw exception in method call Refactor Converter to use the CSVUtils class

--- src/main/Converter.java
+++ src/main/Converter.java
@@ -9,6 +9,7 @@ import java.io.*;
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,7 +18,7 @@ public class Converter {
     List<Auditoria> auditorias = new ArrayList<>();
     File auditTxt;
 
-    public void convert() {
+    public void convert() throws IOException {
         auditTxt = getAuditTxt();
         addAuditorias(auditTxt);
         exportToCsv();
@@ -68,40 +69,63 @@ public class Converter {
         auditorias.add(auditoria);
     }
 
-    private void exportToCsv() {
+    private void exportToCsv() throws IOException {
         String[] auditRoute = auditTxt.getAbsolutePath().split("\\.");
         String route = auditRoute[0];
-        try {
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(route + ".csv"), StandardCharsets.UTF_8))) {
-                String header = "Cuenta,Descripcion,Entidad,Act,Saldo Apertura,Saldo Inicial,Debito,Credito,Saldo Final";
-                bufferedWriter.write(header);
-                bufferedWriter.newLine();
-                for (Auditoria auditoria : auditorias) {
-                    String oneLine = auditoria.getCuenta() +
-                            CSV_SEPARATOR +
-                            auditoria.getDescripcion() +
-                            CSV_SEPARATOR +
-                            auditoria.getEntidad() +
-                            CSV_SEPARATOR +
-                            auditoria.getAct() +
-                            CSV_SEPARATOR +
-                            auditoria.getSaldoApertura() +
-                            CSV_SEPARATOR +
-                            auditoria.getSaldoInicial() +
-                            CSV_SEPARATOR +
-                            auditoria.getDebito() +
-                            CSV_SEPARATOR +
-                            auditoria.getCredito() +
-                            CSV_SEPARATOR +
-                            auditoria.getSaldoFinal();
-                    bufferedWriter.write(oneLine);
-                    bufferedWriter.newLine();
-                }
-                bufferedWriter.flush();
-            }
-            JOptionPane.showMessageDialog(null, "CSV generado en " + route + ".csv");
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
+        FileWriter writer = new FileWriter(route + ".csv");
+
+        CSVUtils.writeLine(writer, Arrays.asList("Cuenta", "Descripcion", "Entidad", "Act", "Saldo Apertura"
+                , "Saldo Inicial", "Debito", "Credito", "Saldo Final"));
+
+        for (Auditoria auditoria : auditorias) {
+            List<String> list = new ArrayList<>();
+            list.add(auditoria.getCuenta());
+            list.add(auditoria.getDescripcion());
+            list.add(auditoria.getEntidad());
+            list.add(auditoria.getAct());
+            list.add(auditoria.getSaldoApertura().toString());
+            list.add(auditoria.getSaldoInicial().toString());
+            list.add(auditoria.getDebito().toString());
+            list.add(auditoria.getCredito().toString());
+            list.add(auditoria.getSaldoFinal().toString());
+
+            CSVUtils.writeLine(writer, list);
         }
+
+        writer.flush();
+        writer.close();
+
+//        try {
+//            try (BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(route + ".csv"), StandardCharsets.UTF_8))) {
+//                String header = "Cuenta,Descripcion,Entidad,Act,Saldo Apertura,Saldo Inicial,Debito,Credito,Saldo Final";
+//                bufferedWriter.write(header);
+//                bufferedWriter.newLine();
+//                for (Auditoria auditoria : auditorias) {
+//                    String oneLine = auditoria.getCuenta() +
+//                            CSV_SEPARATOR +
+//                            auditoria.getDescripcion() +
+//                            CSV_SEPARATOR +
+//                            auditoria.getEntidad() +
+//                            CSV_SEPARATOR +
+//                            auditoria.getAct() +
+//                            CSV_SEPARATOR +
+//                            auditoria.getSaldoApertura() +
+//                            CSV_SEPARATOR +
+//                            auditoria.getSaldoInicial() +
+//                            CSV_SEPARATOR +
+//                            auditoria.getDebito() +
+//                            CSV_SEPARATOR +
+//                            auditoria.getCredito() +
+//                            CSV_SEPARATOR +
+//                            auditoria.getSaldoFinal();
+//                    bufferedWriter.write(oneLine);
+//                    bufferedWriter.newLine();
+//                }
+//                bufferedWriter.flush();
+//            }
+//            JOptionPane.showMessageDialog(null, "CSV generado en " + route + ".csv");
+//        } catch (IOException e) {
+//            JOptionPane.showMessageDialog(null, e.getMessage());
+//        }
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,10 @@
 package main;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Converter converter = new Converter();
         converter.convert();
     }
